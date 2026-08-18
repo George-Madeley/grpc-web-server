25130777b40693adefb14df78b94095a9f6d7229
George Madeley <george.madeley@outlook.com>
2026-08-18 22:03:08 +0100

    build: added cbindgen

---
+++ build.rs
@@ -0,0 +1,27 @@
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let manifest_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
+    let out_dir = std::path::PathBuf::from(format!("{}/gen/include", manifest_dir));
+
+    if out_dir.exists() {
+        std::fs::remove_dir_all(out_dir.clone())?;
+    }
+    std::fs::create_dir_all(out_dir.clone())?;
+
+    let mut config = cbindgen::Config::default();
+    config.pragma_once = true;
+    config.namespace = Some(String::from("grpc_web_proxy"));
+    config.braces = cbindgen::Braces::NextLine;
+    config.language = cbindgen::Language::C;
+    config.line_length = 120;
+    config.documentation_style = cbindgen::DocumentationStyle::C;
+    config.cpp_compat = true;
+
+    cbindgen::Builder::new()
+        .with_crate(manifest_dir)
+        .with_config(config)
+        .generate()
+        .expect("Unable to generate bindings")
+        .write_to_file(out_dir.join("grpc_web_proxy.h"));
+
+    Ok(())
+}
